72a7171f3b3ed62eb8a70ebdf1f5e3c4e7def711
os <liufuling14@163.com>
2019-04-18 19:05:27 +0800

    20190418

---
+++ untitled/src/Cat.java
@@ -0,0 +1,13 @@
+public class Cat extends Animal {
+    public void eat(){
+        System.out.println("猫吃鱼");
+
+    }
+
+    @Override
+    public void sport() {
+        System.out.println("毛和老鼠捉迷藏");
+
+    }
+
+}
